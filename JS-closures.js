2cf5a277eb4c662ab0b26baf082a7b916b91b1a9
Hunter Chapman <bootcoder@gmail.com>
2014-10-15 21:45:25 -0700

    basic closure example for variable scope

--- JS-closures.js
+++ JS-closures.js
@@ -1,5 +1,18 @@
-function init(){
-	console.log("Hello World");
-}
+counter = 5
+var privateNum = (
+function() {
+	var counter = 0;
+	return function() {
+		return counter ++;
+	};
+}()
+);
 
-window.onload = init
+
+document.write(counter + '<br/>'); //exterior var remains untouched
+
+document.write(privateNum() + '<br/>'); // interior var has interior scope and 
+document.write(privateNum() + '<br/>'); // is incremented independent of exterior
+document.write(privateNum() + '<br/>');
+
+document.write(counter + '<br/>'); //exterior var remains untouched
