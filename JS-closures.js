52c5179e379a7f116e05a11abc61c6abf0d6cefb
Hunter Chapman <bootcoder@gmail.com>
2014-10-16 23:03:51 -0700

    add fibonacci sequence initial code

--- JS-closures.js
+++ JS-closures.js
@@ -9,6 +9,13 @@ function() {
 );
 
 
+ var text = 'outside';
+	function logIt(){
+	    console.log(text);
+	    var text = 'inside';
+	};
+	logIt();
+
 document.write(counter + '<br/>'); //exterior var remains untouched
 
 document.write(privateNum() + '<br/>'); // interior var has interior scope and 
@@ -16,3 +23,36 @@ document.write(privateNum() + '<br/>'); // is incremented independent of exterio
 document.write(privateNum() + '<br/>');
 
 document.write(counter + '<br/>'); //exterior var remains untouched
+
+
+
+(function() {
+  console.log(1)
+  setTimeout(function(){console.log(2)}, 100)
+  setTimeout(function(){console.log(3)}, 0)
+  console.log(4)
+})()
+
+
+
+
+var charTime; // charTime===35
+function someTypeAheadFunction (val) {
+  var ajaxCall = function(val){
+    // $.ajax().done()
+  } 
+  
+  clearTimeout(charTime); // => clearTimeout(24)
+  charTime = setTimeout(function(value){
+    ajaxCall(value);
+  },300) // => chartime === 35
+  
+
+ 
+};
+
+var fib = [0, 1];
+for (var i = 2; i < n; i++) {
+    fib[ i ] = fib[ i - 1 ] + fib[ i - 2 ];
+    console.log(fib);
+}
